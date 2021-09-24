046a76ab41778838b5a291c88a2ba6dec0dfb915
Stefan Manciu <manciustefan@gmail.com>
2021-09-24 17:30:37 +0300

    Added dashboard

--- btsc.py
+++ btsc.py
@@ -5,13 +5,14 @@ import requests
 import typer
 import os
 
-from commands import auth, filter, directory
+from commands import auth, filter, directory, dashboard
 
 app = typer.Typer()
 
 app.add_typer(auth.app, name="auth")
 app.add_typer(filter.app, name="filter")
 app.add_typer(directory.app, name="directory")
+app.add_typer(dashboard.app, name="dashboard")
 
 if __name__ == "__main__":
     app()
