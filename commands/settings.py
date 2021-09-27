d59b5d386413b360e07e06378f577e78c8366920
Stefan Manciu <manciustefan@gmail.com>
2021-09-27 16:57:32 +0300

    Added get and set commands for settings

--- commands/settings.py
+++ commands/settings.py
@@ -10,13 +10,32 @@ app = typer.Typer()
 
 state = {
     'accessToken': None,
-    'providerId': None
+    'providerId': None,
+    'wallet': None
 }
 
 class Action(str, Enum):
     Filtering = "filtering",
     Sharing = "sharing",
     Importing = "importing"
+
+class ProviderDataKey(str, Enum):
+    Country = "country",
+    BusinessName = "name",
+    Website = "website",
+    Email = "email",
+    ContactPerson = "contact-person",
+    Address = "address"
+
+def mapProviderDataKey(key: ProviderDataKey):
+    return {
+        ProviderDataKey.Country: "country",
+        ProviderDataKey.BusinessName: "businessName",
+        ProviderDataKey.Website: "website",
+        ProviderDataKey.Email: "email",
+        ProviderDataKey.ContactPerson: "contactPerson",
+        ProviderDataKey.Address: "address",
+    }[key]
 
 def setValue(action: Action, value: bool):
     response = requests.get(f"{host}/config/{state['providerId']}", auth=BearerAuth(state['accessToken']))
@@ -62,10 +81,40 @@ def disable(
 ):
     setValue(action, False)
 
+@app.command(name="set")
+def setProviderData(
+    key: ProviderDataKey = typer.Argument(..., case_sensitive=False),
+    value: str = typer.Argument(...)
+):
+    response = requests.get(f"{host}/provider/{state['wallet']}")
+    provider = response.json()
+
+    provider[mapProviderDataKey(key)] = value
+    provider['walletAddress'] = state['wallet']
+    response = requests.put(f"{host}/provider", json=provider, auth=BearerAuth(state['accessToken']))
+    if response.status_code == 200:
+        typer.secho("Done.", bg=typer.colors.GREEN, fg=typer.colors.BLACK)
+    else:
+        typer.secho("Error: ", bg=typer.colors.RED)
+        print(response.json())
+
+@app.command()
+def get():
+    response = requests.get(f"{host}/provider/{state['wallet']}")
+    provider = response.json()
+
+    typer.secho(f"Business Name: {provider['businessName']}")
+    typer.secho(f"Country: {provider['country']}")
+    typer.secho(f"Website: {provider['website']}")
+    typer.secho(f"Email: {provider['email']}")
+    typer.secho(f"Contact Person: {provider['contactPerson']}")
+    typer.secho(f"Address: {provider['address']}")
+
 @app.callback()
 def getAuthData():
     state['accessToken'] = getConfigFromFile('access_token')
     state['providerId'] = getConfigFromFile('provider_id')
+    state['wallet'] = getConfigFromFile('eth_wallet')
 
     if state['accessToken'] is None or state['providerId'] is None:
         raise typer.Exit("Not logged in.")
